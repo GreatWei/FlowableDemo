ef1d09c9ce35bf5c2cf6dd5a9a601c46a714473f
ZHANGZHIWEI <1121412775@qq.com>
2022-02-22 19:04:09 +0800

    1

--- src/main/java/com/example/FlowableDemo/FlowableDemoApplication.java
+++ src/main/java/com/example/FlowableDemo/FlowableDemoApplication.java
@@ -1,7 +1,12 @@
 package com.example.FlowableDemo;
 
+import org.flowable.engine.RepositoryService;
+import org.flowable.engine.RuntimeService;
+import org.flowable.engine.TaskService;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class FlowableDemoApplication {
@@ -9,5 +14,4 @@ public class FlowableDemoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(FlowableDemoApplication.class, args);
 	}
-
 }
